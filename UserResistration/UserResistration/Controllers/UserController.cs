97e52dfe0b1f7f06a3146ed5d3e2e78b15882ca9
Cesar <devilonline@gmail.com>
2019-10-10 02:57:26 +0100

    actualizaçao dos ficheiros
    coloquei os ficheiros iniciais

---
+++ UserResistration/UserResistration/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UserResistration.Models;
+
+namespace UserResistration.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        [HttpGet]
+        public ActionResult AddOrEdit(int id=0)
+        {
+            dbo_User userModel = new dbo_User();
+
+            return View(userModel);
+        }
+        [HttpPost]
+        public ActionResult AddorEdit (dbo_User userModel)
+        {
+            using (DbModels dbModel = new DbModels())
+            {
+                dbModel.dbo_User.Add(userModel);
+                dbModel.SaveChanges();
+            }
+            ModelState.Clear();
+            ViewBag.SucessMessage = "Registration Sucessful.";
+            return View("AddorEdit", new dbo_User());
+        }
+    }
+}
